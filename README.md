3154d04595c4d76c2c91cebcc440381a0aa62417
dchotai <dchotai@users.noreply.github.com>
2016-06-26 16:05:00 -0700

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # NBAnames
+Extracts the names of all historical NBA players and writes a text file containing combinations of two players' names, e.g. LeBron James Harden and Chris Paul George. NBA player data is extracted from http://www.basketball-reference.com/ 
+
+Run:
+```
+python nbanames.py
+```
---
+++ nbanames.py
@@ -0,0 +1,47 @@
+# Made by Divyesh Chotai 2016
+
+import re
+import urllib
+
+"""
+Extracts the names of all historical NBA players and writes a text file containing combinations of two players' names, 
+e.g. LeBron James Harden and Chris Paul George. NBA player data is extracted from http://www.basketball-reference.com/
+"""
+
+results=[]
+
+# Extract all the names and store into a list named 'results'
+def extract_names():
+  letters = 'abcdefghijklmnopqrstuvwyz'
+  for each in letters:
+    bballreference = 'http://www.basketball-reference.com/players/%s' % (each)
+    ufile = urllib.urlopen(bballreference)
+
+    reader = ufile.read()
+
+    names = re.findall(r'<a href="/players/.+.html">(.+\s.+)</a>', reader)
+
+    for name in names:
+      results.append(name)
+
+
+# Split each name into first and last name, then look for all name combinations and return them
+def intersect_names():
+  namesplit = [item.split() for item in results]
+  final = [''.join(x[0] + ' ' + x[1] + ' ' + y[1]) for x in namesplit for y in namesplit if x[1]==y[0]]
+  return final
+
+def main():
+  print 'Extracting names...'
+  extracted = extract_names()
+  print 'Names extracted.'
+
+  intersect = intersect_names()
+  f = open('nbanames.txt', 'w')
+  f.write('\n'.join(intersect))
+  f.close()
+  print 'Merged names written to ' + f.name
+
+
+if __name__ == '__main__':
+  main()
